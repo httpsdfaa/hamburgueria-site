66cf4ac7a2b0204980915b9a974df00ac5dcdc6f
httsdfaa <deivisondfaa@gmail.com>
2024-08-28 11:29:54 -0300

    :package: Configuração do framework tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx}"
+  ],
+  theme: {
+    fontFamily: {
+      "yaLike": ["Love Ya Like A Sister", 'cursive'],
+      "libreFranklin": ["Libre Franklin", 'sans-serif']
+    }
+  },
+  plugins: [],
+}
+
