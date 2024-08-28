d518f25df89d5a78871fe72cc424612d11589c80
httsdfaa <deivisondfaa@gmail.com>
2024-08-28 17:17:15 -0300

    :package: configuração das cores e fontes tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,15 @@ module.exports = {
   ],
   theme: {
     fontFamily: {
-      "yaLike": ["Love Ya Like A Sister", 'cursive'],
-      "libreFranklin": ["Libre Franklin", 'sans-serif']
+      'yaLike': ['Love Ya Like A Sister', 'cursive'],
+      'libreFranklin': ['Libre Franklin', 'sans-serif']
+    },
+    colors: {
+      'marromAvermelhado': '#805557',
+      'titleNavManteiga': '#F6F3E0',
+      'buttomBlue': '#7274BB',
+      'avermelhadoTittle': '#9F2E33',
+      'defaultBlack': '#303030'
     }
   },
   plugins: [],
