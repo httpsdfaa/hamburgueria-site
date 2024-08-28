81e76ed82aa466b506d5fd9173cc66109247dea0
httsdfaa <deivisondfaa@gmail.com>
2024-08-28 11:31:01 -0300

    :construction: Contruindo o header do projeto

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -4,8 +4,12 @@ import "../styles/Header.css"
 
 function Header() {
     return (
-        <header className="header">
-            Header
+        <header className="header flex justify-between">
+            <img src="/" alt="logo"></img>
+            <nav className="navbar font-yaLike">
+                <a href="/">HOME</a>
+                <a href="/">CONTATO</a>
+            </nav>
         </header>
     )
 }
