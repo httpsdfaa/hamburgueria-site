13ce4158b5a9e2752f60b427b98bcc8876798979
httsdfaa <deivisondfaa@gmail.com>
2024-08-30 20:54:28 -0300

    :bug: fix: classe de cor do texto estava em local errado

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -6,9 +6,9 @@ function Header() {
     return (
         <header className="header flex justify-between items-center bg-marromAvermelhado h-20 w-full px-28">
             <img className="rounded-full w-28 h-28 relative top-1/2 z-10" src={Logo} alt="logo"></img>
-            <nav className="navbar font-yaLike text-titleNavManteiga">
-                <a href="/" className="mr-5">HOME</a>
-                <a href="/" className="ml-5">CONTATO</a>
+            <nav className="navbar font-yaLike">
+                <a href="/" className="mr-5 text-titleNavManteiga">HOME</a>
+                <a href="/" className="ml-5 text-titleNavManteiga">CONTATO</a>
             </nav>
         </header>
     )
