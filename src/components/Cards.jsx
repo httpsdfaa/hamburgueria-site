ce271323f277465a8b8eb2312b4429edc4a36d90
httsdfaa <deivisondfaa@gmail.com>
2024-09-01 09:47:48 -0300

    :construction: Desenvolvendo Cards

---
+++ src/components/Cards.jsx
@@ -0,0 +1,162 @@
+import React from 'react';
+
+import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
+
+import Image from '../assets/sanduiche.png'
+
+function Cards() {
+    const marginPrincipal = 'm-12'
+    const minWidth =  'min-w-96'
+    const maxWidth = 'max-w-xl'
+
+    return (
+        <section className='flex flex-wrap items-center justify-center'>
+            <Card style={{ width: '18rem' }} className={`${marginPrincipal} ${minWidth} ${maxWidth}`}>
+                <Card.Img variant="top" src={Image} />
+                <Card.Body>
+                    <div className="text-avermelhadoTittle font-yaLike">
+                        <Card.Title>Nome_sanduíche</Card.Title>
+                    </div>
+                    <Card.Text>
+                        Alguma descrição digna de aplausos
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Acompanhamento 1</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 2</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 3</ListGroup.Item>
+                    <ListGroup.Item>...</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento N</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <h4 className='text-avermelhadoTittle font-yaLike'>R$ 50,00</h4>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className={`${marginPrincipal} ${minWidth} ${maxWidth}`}>
+                <Card.Img variant="top" src={Image} />
+                <Card.Body>
+                    <div className="text-avermelhadoTittle font-yaLike">
+                        <Card.Title>Nome_sanduíche</Card.Title>
+                    </div>
+                    <Card.Text>
+                        Alguma descrição digna de aplausos
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Acompanhamento 1</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 2</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 3</ListGroup.Item>
+                    <ListGroup.Item>...</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento N</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <h4 className='text-avermelhadoTittle font-yaLike'>R$ 50,00</h4>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className={`${marginPrincipal} ${minWidth} ${maxWidth}`}>
+                <Card.Img variant="top" src={Image} />
+                <Card.Body>
+                    <div className="text-avermelhadoTittle font-yaLike">
+                        <Card.Title>Nome_sanduíche</Card.Title>
+                    </div>
+                    <Card.Text>
+                        Alguma descrição digna de aplausos
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Acompanhamento 1</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 2</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 3</ListGroup.Item>
+                    <ListGroup.Item>...</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento N</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <h4 className='text-avermelhadoTittle font-yaLike'>R$ 50,00</h4>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className={`${marginPrincipal} ${minWidth} ${maxWidth}`}>
+                <Card.Img variant="top" src={Image} />
+                <Card.Body>
+                    <div className="text-avermelhadoTittle font-yaLike">
+                        <Card.Title>Nome_sanduíche</Card.Title>
+                    </div>
+                    <Card.Text>
+                        Alguma descrição digna de aplausos
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Acompanhamento 1</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 2</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 3</ListGroup.Item>
+                    <ListGroup.Item>...</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento N</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <h4 className='text-avermelhadoTittle font-yaLike'>R$ 50,00</h4>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className={`${marginPrincipal} ${minWidth} ${maxWidth}`}>
+                <Card.Img variant="top" src={Image} />
+                <Card.Body>
+                    <div className="text-avermelhadoTittle font-yaLike">
+                        <Card.Title>Nome_sanduíche</Card.Title>
+                    </div>
+                    <Card.Text>
+                        Alguma descrição digna de aplausos
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Acompanhamento 1</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 2</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 3</ListGroup.Item>
+                    <ListGroup.Item>...</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento N</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <h4 className='text-avermelhadoTittle font-yaLike'>R$ 50,00</h4>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+
+            <Card style={{ width: '18rem' }} className={`${marginPrincipal} ${minWidth} ${maxWidth}`}>
+                <Card.Img variant="top" src={Image} />
+                <Card.Body>
+                    <div className="text-avermelhadoTittle font-yaLike">
+                        <Card.Title>Nome_sanduíche</Card.Title>
+                    </div>
+                    <Card.Text>
+                        Alguma descrição digna de aplausos
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Acompanhamento 1</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 2</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento 3</ListGroup.Item>
+                    <ListGroup.Item>...</ListGroup.Item>
+                    <ListGroup.Item>Acompanhamento N</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <h4 className='text-avermelhadoTittle font-yaLike'>R$ 50,00</h4>
+                    <Card.Link href="#">Card Link</Card.Link>
+                    <Card.Link href="#">Another Link</Card.Link>
+                </Card.Body>
+            </Card>
+        </section>
+    );
+}
+
+export default Cards;
--- src/pages/Body.jsx
+++ src/pages/Body.jsx
@@ -3,11 +3,13 @@ import React from "react";
 import "../styles/Body.css"
 // import PhotoBurger from '../assets/sanduiche.png'
 import CarouselSlide from "../components/Carousel";
+import Cards from '../components/Cards';
 
 function Body() {
     return (
         <main className="body">
             <CarouselSlide />
+            <Cards />
         </main>
     )
 }
