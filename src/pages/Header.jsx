93166353d185e13344fd3a33b5dbc90b66baed1a
httsdfaa <deivisondfaa@gmail.com>
2024-09-02 21:26:29 -0300

    :construction: Adicionando botão menu

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,16 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Logo from "../assets/logo.png"
+import MenuList from "../assets/icons/MenuList";
 import '../styles/Header.css'
 
+
 function Header() {
+
+    const [toggle, setToggle] = useState(true ? window.innerWidth <= 600 : false);
+
+    // const onclick = () => {
+    //     toggle ? setToggle(true) : setToggle(false)
+    // }
+
+    // window.addEventListener('resize', () => {
+    //     (window.innerWidth <= 600) ? setToggle(true) : setToggle(false)
+    // })
+
     return (
         <header className="header flex justify-between items-center bg-marromAvermelhado h-20 w-full px-28">
             <img className="img-logo rounded-full w-28 h-28 relative top-1/2 z-10" src={Logo} alt="logo"></img>
-            <nav className="navbar font-yaLike">
-                <a href="/" className="mr-5 text-titleNavManteiga">HOME</a>
-                <a href="/" className="ml-5 text-titleNavManteiga">CONTATO</a>
-            </nav>
+
+            <MenuList className='menu-list' onclick={onclick} />
+
+            {
+                toggle ?
+
+                <nav className="navbar font-yaLike">
+                    <a href="/" className="mr-5 text-titleNavManteiga">HOME</a>
+                    <a href="/" className="ml-5 text-titleNavManteiga">CONTATO</a>
+                </nav>
+
+                : null
+            }
         </header>
     )
 }
