6083169068870cd2ea0c28c8f9c44badd67cc28f
httsdfaa <deivisondfaa@gmail.com>
2024-09-05 22:29:45 -0300

    :construction: :wrench: Configurando mapa / api do google

--- frontend/src/components/Maps.tsx
+++ frontend/src/components/Maps.tsx
@@ -1,50 +1,49 @@
+import 'dotenv/config'
 import React from 'react';
+
+
+// A DOCUMENTAÇÃO FOI SEGUIDA DO SITE DEVELOPERS GOOGLE MAPS
 import {
-    APIProvider,
-    Map,
+    APIProvider, // Faz parte de Map
+    Map, // Permite alterar o mapa
     AdvancedMarker,
     MapCameraChangedEvent,
     Pin
 } from '@vis.gl/react-google-maps';
 
+// Faz parte dos marcadores do mapa
 type Poi = { key: string, location: google.maps.LatLngLiteral }
 const locations: Poi[] = [
-    { key: 'operaHouse', location: { lat: -33.8567844, lng: 151.213108 } },
-    { key: 'tarongaZoo', location: { lat: -33.8472767, lng: 151.2188164 } },
-    { key: 'manlyBeach', location: { lat: -33.8209738, lng: 151.2563253 } },
-    { key: 'hyderPark', location: { lat: -33.8690081, lng: 151.2052393 } },
-    { key: 'theRocks', location: { lat: -33.8587568, lng: 151.2058246 } },
-    { key: 'circularQuay', location: { lat: -33.858761, lng: 151.2055688 } },
-    { key: 'harbourBridge', location: { lat: -33.852228, lng: 151.2038374 } },
-    { key: 'kingsCross', location: { lat: -33.8737375, lng: 151.222569 } },
-    { key: 'botanicGardens', location: { lat: -33.864167, lng: 151.216387 } },
-    { key: 'museumOfSydney', location: { lat: -33.8636005, lng: 151.2092542 } },
-    { key: 'maritimeMuseum', location: { lat: -33.869395, lng: 151.198648 } },
-    { key: 'kingStreetWharf', location: { lat: -33.8665445, lng: 151.1989808 } },
-    { key: 'aquarium', location: { lat: -33.869627, lng: 151.202146 } },
-    { key: 'darlingHarbour', location: { lat: -33.87488, lng: 151.1987113 } },
-    { key: 'barangaroo', location: { lat: - 33.8605523, lng: 151.1972205 } },
+    { key: 'operaHouse', location: { lat: -19.75599193269231, lng: -43.99126695894421 } }
 ];
+
+// Cordenadas do centro na qual queremos focar
+const cordMaps = {
+    lat: -19.75599193269231,
+    lng: -43.99126695894421
+}
 
 function mapsGoogle() {
     return (
         <APIProvider
-            apiKey={'AIzaSyDlQbDE1WrTCSfzCoHDUSf4Fnbd9gHb0dQ'} // TIRAR NÃO DEIXAR
-            onLoad={() => console.log('Maps API has loaded.')}
+            apiKey={`${process.env.REACT_APP_KEY_API}`} // Chave Api google
+            onLoad={() => console.log('Maps API has loaded.')}  // Verificando se carregou
         >
             <Map
-                defaultZoom={15}
-                defaultCenter={{ lat: -33.860664, lng: 151.208138 }}
+                defaultZoom={15} // Zoom onde localiza o centro
+                defaultCenter={cordMaps} // Cordenadas do centro desejado
+                mapId={'65559ab5179aaa7'} // Id do mapa criado no console do google
                 onCameraChanged={(ev: MapCameraChangedEvent) =>
                     console.log('camera changed:', ev.detail.center, 'zoom:', ev.detail.zoom)
                 }
             >
-                <PoiMarkers pois={locations} />
+                <PoiMarkers pois={locations} /> {/*Marcador do mapa*/}
             </Map>
         </APIProvider>
     )
 }
 
+// Componente para montar o marcador do mapa
 const PoiMarkers = (props: { pois: Poi[] }) => {
     return (
         <>
