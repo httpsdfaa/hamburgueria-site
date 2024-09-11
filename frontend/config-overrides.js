c4496ad2af9fa70f6f9e565b2d960057a898ceb2
httsdfaa <deivisondfaa@gmail.com>
2024-09-11 20:09:49 -0300

    :wrench: configuração do webpack

--- frontend/config-overrides.js
+++ frontend/config-overrides.js
@@ -1,16 +1,24 @@
 // config-overrides.js
 const webpack = require('webpack');
+const path = require('path')
 
 module.exports = function override(config) {
+
   // Adiciona os fallbacks para polyfills
-  config.resolve.fallback = {
-    "path": require.resolve("path-browserify"),
-    "crypto": require.resolve("crypto-browserify"),
-    "os": require.resolve("os-browserify/browser"),
-    "process": require.resolve("process/browser"),
-    "buffer": require.resolve("buffer/"),
-    "stream": require.resolve("stream-browserify"),
-    "vm": require.resolve('vm-browserify')
+  config.resolve = {
+    extensions: ['.js', '.jsx'], // extensão necessário 
+    fallback: {
+      "path": require.resolve("path-browserify"),
+      "crypto": require.resolve("crypto-browserify"),
+      "os": require.resolve("os-browserify/browser"),
+      "process": require.resolve("process/browser"),
+      "buffer": require.resolve("buffer/"),
+      "stream": require.resolve("stream-browserify"),
+      "vm": require.resolve('vm-browserify')
+    },
+    alias: {
+      'process/browser': path.resolve(__dirname, 'node_modules/process/browser.js')
+    }
   };
 
   // Adiciona o ProvidePlugin para fornecer process e Buffer globalmente
