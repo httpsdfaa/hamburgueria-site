798df64090ee833caedd8ce8801ba25cb9c3bb4c
httsdfaa <deivisondfaa@gmail.com>
2024-09-11 20:10:16 -0300

    :wrench: construção e configuração dos dados

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,34 @@ const dotenv = require('dotenv')
 const path = require('path')
 dotenv.config({ path: path.resolve(__dirname, './config/.env') })
 
+const data = require('./data-products/data')
+
 // Db
-const db = require('./database/dbConnection')
-console.log(db)
+// const db = require('./database/dbConnection')
+// console.log(db)
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello World')
 })
 
+app.get('/products', (req, res) => {
+    const sanduiches_product = data.sanduiches
+    const bebidas_product= data.bebidas
+
+    const products = { sanduiches: sanduiches_product, bebidas: bebidas_product }
+
+    if(products){
+        res.json(products)
+    } else {
+        res.status(404).json({message: 'Produto não encontrado'})
+    }
+})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
