56fbada855f384cce9d257f1678c4784b8646843
httsdfaa <deivisondfaa@gmail.com>
2024-09-11 12:48:13 -0300

    :construction: desenvolvendo o carrinho

--- frontend/src/components/Cart.jsx
+++ frontend/src/components/Cart.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import '../styles/Cart.css'
+import React from 'react';
+import '../styles/Cart.css';
 
 const Cart = () => {
     return (
-        <section className="section-cart absolute top-0 h-full" id="section-cart">
+        <section className="section-cart flex absolute top-0 h-full " id="section-cart">
             <p>Sacola</p>
         </section>
     );
--- frontend/src/pages/Header.jsx
+++ frontend/src/pages/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Logo from "../assets/logo.png"
 import MenuList from "../assets/icons/MenuList";
@@ -12,6 +12,8 @@ import '../styles/Header.css'
 function Header() {
 
     const [toggle, setToggle] = useState(true ? window.innerWidth > 600 : false);
+    const [isActive, setActive] = useState(false)
+    const navigate = useNavigate();
 
     // width maior 600 redenrizar navbar
     window.addEventListener('resize', () => {
@@ -31,12 +33,19 @@ function Header() {
         }
     }
 
+    const openCart = () => {
+        navigate('/carrinho')
+        
+        // Desativa barra de rolagem e focar no carrinho
+        document.body.style.overflow = 'hidden';
+    }
+
     return (
         <header className="header flex justify-between items-center bg-marromAvermelhado h-20 w-full px-20">
-            <div className="div-cart">
-                {/* Routa para acessar carrinho */}
-                <Link to="/carrinho"><a><CartIcon /></a></Link>
-            </div>
+            {/* Botão para navegar para a página carrinho */}
+            <button onClick={openCart} className="div-cart">
+                <CartIcon />
+            </button>
             <img className="img-logo rounded-full w-28 h-28 relative top-1/2 z-30" src={Logo} alt="logo"></img>
             {// Caso navbar esteja não redenrizado mostrar icon menu
                 !toggle ?
