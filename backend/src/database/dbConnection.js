0e00f708d995cba85f048951480f1f460bb22811
httsdfaa <deivisondfaa@gmail.com>
2024-09-10 21:16:59 -0300

    :construction: Construção do projeto

--- backend/src/database/dbConnection.js
+++ backend/src/database/dbConnection.js
@@ -1,5 +1,4 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const fs = require('fs')
 const dotenv = require('dotenv')
 const path = require('path')
 
@@ -18,72 +17,12 @@ const client = new MongoClient(uri, {
     }
 });
 
-async function crud() {
-
-    const db = client.db('db_burger');
-    const collection = db.collection('teste_server');
-
-    try {
-        // await collection.insertOne(
-        //     {
-        //         name: 'Deivison3',
-        //         age: 26,
-        //         status: 'teste de create'
-        //     }
-        // )
-
-        const dados = await collection.find({
-            name: { $in: ["Deivison", "Deivison2"] } // Recupara os dois dados
-        }).toArray(); // Sempre colocar como array para que trate em formato Array e não BSON
-
-        return dados
-    } finally {
-        await client.close()
-    }
-}
-
-async function storeImage() {
-    try {
-        const db = client.db('db_burger');
-        const collection = db.collection('image_data');
-
-        const imageBanner = `D:\projects\hamburgueria-site\backend\assets\banner-bg.png`
-        const imageCocaCola = 'D:\projects\hamburgueria-site\backend\assets\coca-cola.jpg'
-        const imageLogo = 'D:\projects\hamburgueria-site\backend\assets\logo.png'
-        const Imagesanduiche = 'D:\projects\hamburgueria-site\backend\assets\sanduiche.png'
-
-        const imagePathArray = [imageBanner, imageCocaCola, imageLogo, Imagesanduiche]
-
-        for (let i = 0; i < imagePathArray; i++) {
-            const imageData = fs.readFileSync(imagePathArray)
-
-            const result = await collection.insertOne({
-                name: 'image-site',
-                data: imageData
-            })
-
-            return result
-        }
-
-    } finally {
-        await client.close()
-    }
-}
-
-
-
-
-
 async function run() {
     try {
         // conectar o cliente no servidor
         await client.connect();
         console.log('Conectado ao Mongo Atlas')
 
-        // const dados = await crud();
-        // console.log('dados encontrados: ', dados)
-
-        await storeImage()
     } finally {
         // Ensures that the client will close when you finish/error
         await client.close();
