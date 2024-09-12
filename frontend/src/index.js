55a97f9c8e66d650c453d48dd5a0e3cd689d395c
httsdfaa <deivisondfaa@gmail.com>
2024-09-12 17:39:20 -0300

    :bug: fix: corrigindo erro de importação não suportada

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import * as ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
